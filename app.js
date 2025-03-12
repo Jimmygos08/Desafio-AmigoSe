ad642b0905f07aabe7b72a6fa3119f8a5c638b38
Jimmygos08 <jimmygs8193@gmail.com>
2025-03-12 05:03:15 -0600

    Requerimientos de Alura

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Requerimientos de alura, deben poder ingresar nombres, realizar un sorteo y arrojar un nombre aleatorio quien sera el amigo secreto.
+
+//Debe permitir agregar nombre mediante el boton añadir.
+//Debe tener validacion de datos de entrada, si intentan ingresar un campo vacio debe aparecer un alert quen indique que se ingrese un nombre valido.
+//Al hacer click en el boton "sortear amigo", la aplicacion debera escoger un nombre aleatorio y mostralo en pantalla.
+
+
 let nombreIngresado = datos;
 
 alert('hola!')
