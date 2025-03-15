212c457f1eee0cbee9f8ff5a38c87d35ad6f497f
Jimmygos08 <jimmygs8193@gmail.com>
2025-03-15 03:18:07 -0600

    Lista de amigos

--- app.js
+++ app.js
@@ -6,7 +6,48 @@
 //Debe tener validacion de datos de entrada, si intentan ingresar un campo vacio debe aparecer un alert quen indique que se ingrese un nombre valido.
 //Al hacer click en el boton "sortear amigo", la aplicacion debera escoger un nombre aleatorio y mostralo en pantalla.
 
+//Base de datos de nombres ingresados.
+let nombreIngresado = [];
 
-let nombreIngresado = datos;
+function agregarAmigo() {
 
-alert('hola!')
+        const amigoIngresado = document.getElementById("amigo");
+        const nombre = amigoIngresado.value.trim();
+
+        if (nombre) {
+            nombreIngresado.push(nombre);
+            
+            input.value = ""; //limpiara la caja donde se ingresan los nombres
+            actualizarLista();
+        }else{
+            alert("Ingrese un nombre valido 1");
+        }
+
+}
+
+// Funcion para actualizar la lista en pantalla
+function actualizarLista(){
+     const lista = document.getElementById("listaAmigos");
+     lista.innerHTML = "";
+     nombreIngresado.forEach((nombre, index)=> {
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+     });
+
+
+}
+
+// Funcion para sortear los nombres ingresados aleatoriamente
+function sortearAmigo(){
+    if (nombreIngresado.length === 0) {
+        alert("Ingrese un nombre valido");
+        return;
+    }
+
+     const listaAmigos = Math.floor(Math.random() * nombreIngresado.length);
+     const nombreGanador = nombreIngresado[listaAmigos];
+     document.getElementById("resultado").textContent = "El amigo ganador es: " + nombreGanador;
+
+
+}
